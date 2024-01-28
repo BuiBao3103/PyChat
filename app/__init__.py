ff4b9a5e5a654e19be877e22288d683923c8e497
baobui3103 <hongbao2003@gmail.com>
2024-01-29 02:53:42 +0700

    refactor code

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+from flask_babel import Babel
 
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -15,8 +16,8 @@ bc = Bcrypt(app)  # flask-bcrypt
 
 # lm = LoginManager()  # flask-loginmanager
 # lm.init_app(app)  # init the login manager
-
-# from app.routes import routes_bp
-#
-# app.register_blueprint(routes_bp)
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+from app import routes
 
--- app/config.py
+++ app/config.py
@@ -8,7 +8,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     CSRF_ENABLED = True
-
+    LANGUAGES = ['en', 'vi']
     # Set up the App SECRET_KEY
     SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_007')
 
