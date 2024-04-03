2da389eb554a8df2855230eec3018da6638c2139
baobui3103 <hongbao2003@gmail.com>
2024-04-03 20:01:42 +0700

    fix: fix import message controller

--- server/app/controllers/__init__.py
+++ server/app/controllers/__init__.py
@@ -6,6 +6,7 @@ from server.app.controllers.auth_controller import AuthController
 from server.app.controllers.user_controller import UserController
 from server.app.controllers.view_controller import ViewController
 from server.app.controllers.conversation_controller import ConversationController
+from server.app.controllers.message_controller import MessageController
 # Additional initialization code for the controllers package, if needed
 # For example, you might set up some global configurations here
 
