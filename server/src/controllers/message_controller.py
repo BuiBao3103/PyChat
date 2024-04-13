46977d2b1918883460ceedcfa03c8be64b52e79b
baobui3103 <hongbao2003@gmail.com>
2024-04-13 18:27:42 +0700

    fix: add fix conversation not found

--- server/src/controllers/message_controller.py
+++ server/src/controllers/message_controller.py
@@ -1,7 +1,7 @@
 from flask import jsonify, request
 from src import db, app
 from src.errors import InvalidAPIUsage
-from src.models import Message, MessageType
+from src.models import Message, MessageType, Conversation
 from datetime import datetime
 from flask import request, make_response
 from src.util.api_features import APIFeatures
@@ -11,6 +11,10 @@ from flask_restful import Resource
 
 class ConversationMessages(Resource):
     def get(self, conversation_id):
+        conversation= Conversation.query.get(conversation_id)
+        if not conversation:
+            raise InvalidAPIUsage(
+                message='Conversation not found', status_code=404)
         query = db.session.query(Message).filter(
             Message.conversation_id == conversation_id)
         api_freatures = APIFeatures(Message, request.args)
