4ed889f4a1db750a19dd303d92611564c71283a8
baobui3103 <hongbao2003@gmail.com>
2024-04-13 18:08:12 +0700

    feat: get one and get all conversations

--- server/src/routes/conversation_routes.py
+++ server/src/routes/conversation_routes.py
@@ -1,10 +1,12 @@
 from flask import Blueprint
-from src.controllers import  ConversationMessages
+from src.controllers import ConversationMessages, Conversations
 from flask_restful import Api
 
-conversation_dp = Blueprint('conversation', __name__, url_prefix='/api/v1/conversations')
+conversation_dp = Blueprint(
+    'conversation', __name__, url_prefix='/api/v1/conversations')
 api = Api(conversation_dp)
-api.add_resource(ConversationMessages,'/<int:conversation_id>/messages')
+api.add_resource(Conversations, '/', '/<int:conversation_id>')
+api.add_resource(ConversationMessages, '/<int:conversation_id>/messages')
 # @app.route('/api/v1/conversations', methods=['GET', 'POST'])
 # # @protect()
 # def conversations_api():
